32b4c7aaf66f79f6fbf9d38eb7906f43a7ddb9d7
Lorena Alonso <lorenaalonsop26@gmail.com>
2018-02-01 21:29:14 +0100

    Añadido el metodo getUnsold

--- Auction.java
+++ Auction.java
@@ -43,7 +43,7 @@ public class Auction
             System.out.println(lot.toString());
         }
     }
-    
+
     /**
      * Make a bid for a lot.
      * A message is printed indicating whether the bid is
@@ -60,14 +60,14 @@ public class Auction
             boolean successful = selectedLot.bidFor(new Bid(bidder, value));
             if(successful) {
                 System.out.println("The bid for lot number " +
-                                   lotNumber + " was successful.");
+                    lotNumber + " was successful.");
             }
             else {
                 // Report which bid is higher.
 
                 System.out.println("Lot number: " + lotNumber +
-                                   " already has a bid of: " +
-                                   selectedLot.getHighestBid().getValue());
+                    " already has a bid of: " +
+                    selectedLot.getHighestBid().getValue());
             }
         }
     }
@@ -86,9 +86,9 @@ public class Auction
             // right lot.
             if(selectedLot.getNumber() != lotNumber) {
                 System.out.println("Internal error: Lot number " +
-                                   selectedLot.getNumber() +
-                                   " was returned instead of " +
-                                   lotNumber);
+                    selectedLot.getNumber() +
+                    " was returned instead of " +
+                    lotNumber);
                 // Don't return an invalid lot.
                 selectedLot = null;
             }
@@ -96,10 +96,11 @@ public class Auction
         }
         else {
             System.out.println("Lot number: " + lotNumber +
-                               " does not exist.");
+                " does not exist.");
             return null;
         }
     }
+
     /**
      * Ejercicio 0060
      */
@@ -115,5 +116,20 @@ public class Auction
                 System.out.println("Valor de la puja: " + element.getHighestBid().getValue());
             }
         }
+    }
+
+    /**
+     * Ejercicio 0061
+     */
+    public ArrayList<Lot> getUnsold()
+    {
+        ArrayList<Lot> objetoSinPuja = new ArrayList<>();
+        for(Lot element : lots) {
+            System.out.println(element.toString());
+            if(element.getHighestBid() == null) {
+                objetoSinPuja.add(element);
+            }
+        }
+        return objetoSinPuja;
     }
 }
